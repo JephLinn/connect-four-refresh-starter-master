3cb09de903d03ace9e6200ee96b1b593aa57b04e
Jeffrey Manning <jeffmgpfit@yahoo.com>
2024-12-27 11:10:11 -0500

    Cursor Done

--- class/cursor.js
+++ class/cursor.js
@@ -22,14 +22,54 @@ class Cursor {
     Screen.setBackgroundColor(this.row, this.col, this.cursorColor);
   }
 
-  left() {
-    // Move cursor left
+  left = () => {
+    this.resetBackgroundColor();
+
+    if (this.col > 0) {
+      this.col--
+    }
+
+    this.setBackgroundColor();
+
+    Screen.render()
   }
 
-  right() {
-    // Move cursor right
-  }
+  right = () => {
+    this.resetBackgroundColor();
 
+    if (this.col +1 < this.numCols) {
+      this.col++
+    }
+
+    this.setBackgroundColor();
+
+    Screen.render();
+  }
+  
+  up = () => {
+    this.resetBackgroundColor();
+
+    if (this.row > 0) {
+     this.row--;
+    }
+
+    this.setBackgroundColor();
+
+    Screen.render();
+
+ }
+
+ down = () => {
+   this.resetBackgroundColor();
+
+ if (this.row +1 < this.numRows) {
+   this.row++;
+ }
+
+ this.setBackgroundColor();
+
+ Screen.render();
+ }
 }
 
 
